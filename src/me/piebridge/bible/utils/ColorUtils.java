dab5b8bfd6fc9fd1e2eb79cb500451064872447d
Liu DongMiao <liudongmiao@gmail.com>
2016-07-05 13:43:30 +0800

    use a, r, g instead of number

--- src/me/piebridge/bible/utils/ColorUtils.java
+++ src/me/piebridge/bible/utils/ColorUtils.java
@@ -10,36 +10,47 @@ public class ColorUtils {
 
     private static DecimalFormat DF = new DecimalFormat("#.##");
 
+    private static final int A = 24;
+    private static final int R = 16;
+    private static final int G = 8;
+    private static final int MAX = 0xff;
+
     private ColorUtils() {
 
     }
 
     public static String rgba(int color) {
         // 0xAARRGGBB
-        int a = (color >>> 24) & 0xff;
-        int r = (color >>> 16) & 0xff;
-        int g = (color >>> 8) & 0xff;
+        int a = (color >>> A) & 0xff;
+        int r = (color >>> R) & 0xff;
+        int g = (color >>> G) & 0xff;
         int b = color & 0xff;
         return String.format(Locale.US, "rgba(%d, %d, %d, %s)", r, g, b, DF.format(a / 255.0));
     }
 
     public static String blend(int source, int drop) {
-        int sourceA = (source >>> 24) & 0xff;
-        int sourceR = (source >>> 16) & 0xff;
-        int sourceG = (source >>> 8) & 0xff;
+        int sourceA = (source >>> A) & 0xff;
+        int sourceR = (source >>> R) & 0xff;
+        int sourceG = (source >>> G) & 0xff;
         int sourceB = source & 0xff;
+        if (sourceA == 0) {
+            sourceA = 0xff;
+        }
 
-        int dropA = (drop >>> 24) & 0xff;
-        int dropR = (drop >>> 16) & 0xff;
-        int dropG = (drop >>> 8) & 0xff;
+        int dropA = (drop >>> A) & 0xff;
+        int dropR = (drop >>> R) & 0xff;
+        int dropG = (drop >>> G) & 0xff;
         int dropB = drop & 0xff;
+        if (dropA == 0) {
+            dropA = 0xff;
+        }
 
         // blend: WebKit/Source/platform/graphics/Color.cpp
-        int d = 255 * (sourceA + dropA) - sourceA * dropA;
-        int targetA = d / 255;
-        int targetR = (sourceR * sourceA * (255 - dropA) + 255 * dropA * dropR) / d;
-        int targetG = (sourceG * sourceA * (255 - dropA) + 255 * dropA * dropG) / d;
-        int targetB = (sourceB * sourceA * (255 - dropA) + 255 * dropA * dropB) / d;
+        int d = 0xff * (sourceA + dropA) - sourceA * dropA;
+        int targetA = d / 0xff;
+        int targetR = (sourceR * sourceA * (0xff - dropA) + 0xff * dropA * dropR) / d;
+        int targetG = (sourceG * sourceA * (0xff - dropA) + 0xff * dropA * dropG) / d;
+        int targetB = (sourceB * sourceA * (0xff - dropA) + 0xff * dropA * dropB) / d;
 
         return String.format(Locale.US, "rgba(%d, %d, %d, %s)", targetR, targetG, targetB,
                 DF.format(targetA / 255.0));
