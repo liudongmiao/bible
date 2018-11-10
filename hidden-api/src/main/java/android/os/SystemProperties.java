526dcace2c72ae6b2a5e7914a012c3984f20c5a8
Liu DongMiao <liudongmiao@gmail.com>
2018-11-10 09:39:59 +0800

    hidden-api for SystemProperties

---
+++ hidden-api/src/main/java/android/os/SystemProperties.java
@@ -0,0 +1,20 @@
+package android.os;
+
+/**
+ * {@hide}
+ */
+public class SystemProperties {
+
+    public static String get(String key) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static String get(String key, String def) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void set(String key, String val) {
+        throw new UnsupportedOperationException();
+    }
+
+}
