liudongmiao/bible

commit 74d2192684ae7d42cd0ce0f68dc9007c7265fb91

use human from books table

bible/src/main/java/me/piebridge/bible/activity/AbstractReadingActivity.java
@@ -301,7 +301,7 @@ public abstract class AbstractReadingActivity extends DrawerActivity
                 bundle.putString(CURR, curr);
                 bundle.putString(NEXT, getString(cursor, VersionProvider.COLUMN_NEXT));
                 bundle.putString(PREV, getString(cursor, VersionProvider.COLUMN_PREVIOUS));
-                bundle.putString(HUMAN, getString(cursor, VersionProvider.COLUMN_HUMAN));
+                bundle.putString(HUMAN, application.getHuman(curr));
                 bundle.putByteArray(CONTENT, FileUtils.compress(getString(cursor, VersionProvider.COLUMN_CONTENT)));
                 bundle.putString(OSIS, curr);
 

commit 44bb972548fe6c39e5e63e0008d1a0d2bdb6a0a0

fix bugs introduced in 74d2192

bible/src/main/java/me/piebridge/bible/activity/AbstractReadingActivity.java
@@ -301,7 +301,7 @@ public abstract class AbstractReadingActivity extends DrawerActivity
                 bundle.putString(CURR, curr);
                 bundle.putString(NEXT, getString(cursor, VersionProvider.COLUMN_NEXT));
                 bundle.putString(PREV, getString(cursor, VersionProvider.COLUMN_PREVIOUS));
-                bundle.putString(HUMAN, application.getHuman(curr));
+                bundle.putString(HUMAN, application.getHuman(BibleUtils.getBook(curr)));
                 bundle.putByteArray(CONTENT, FileUtils.compress(getString(cursor, VersionProvider.COLUMN_CONTENT)));
                 bundle.putString(OSIS, curr);
 