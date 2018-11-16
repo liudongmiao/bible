8a1774fb395f04b1641c13e26a9ee7d170f22204
Liu DongMiao <liudongmiao@gmail.com>
2018-11-16 16:10:10 +0800

    don't auto select previus chapter

--- bible/src/main/java/me/piebridge/bible/activity/SelectActivity.java
+++ bible/src/main/java/me/piebridge/bible/activity/SelectActivity.java
@@ -189,16 +189,11 @@ public class SelectActivity extends ToolbarActivity
 
     public void setBook(String book) {
         this.book = book;
-        this.chapter = PreferenceManager.getDefaultSharedPreferences(this).getString(book, null);
-        if (TextUtils.isEmpty(this.chapter)) {
-            Map<String, String> chapters = prepareChapters(book);
-            this.chapter = chapters.keySet().iterator().next();
-            selectChapter.setItems(chapters, this.chapter);
-            updateTitle(CHAPTER);
-            mPager.setCurrentItem(CHAPTER);
-        } else {
-            finishSelect();
-        }
+        Map<String, String> chapters = prepareChapters(book);
+        this.chapter = chapters.keySet().iterator().next();
+        selectChapter.setItems(chapters, this.chapter);
+        updateTitle(CHAPTER);
+        mPager.setCurrentItem(CHAPTER);
     }
 
     public void setChapter(String chapter) {
